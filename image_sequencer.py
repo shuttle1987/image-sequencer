a0410081db1eec299bb0c35d90ddd1b9eb62c526
Janis <consulting@lesinskis.com>
2024-10-04 23:24:26 +1000

    Add error handling case

--- image_sequencer.py
+++ image_sequencer.py
@@ -7,13 +7,18 @@ import moviepy.video.io.ImageSequenceClip
 parser = argparse.ArgumentParser(description='Process image files into a video')
 parser.add_argument("--fps", type=float, help="How many frames per second", default=1)
 parser.add_argument("--output", type=str, help="Output filename", default='my_video.mp4')
+parser.add_argument("--input", type=str, help="Input directory", default='input-images')
 args = parser.parse_args()
 
-image_folder: Path = Path('test')
+
+image_folder: Path = Path(args.input)
 fps: float = args.fps
 output_filename = args.output
 
 image_files = [str(img) for img in image_folder.glob("*.png")]
+
+if not image_files:
+    raise ValueError(f"Didn't find any input image files in folder '{image_folder}'... aborting")
 
 print(image_files)
 image_files.sort()
