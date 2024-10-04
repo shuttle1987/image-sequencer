e2e9d74937d95d2956b657c536b9bdf364e533f6
Janis <consulting@lesinskis.com>
2024-10-04 23:15:38 +1000

    Allow specification of output filename

--- image_sequencer.py
+++ image_sequencer.py
@@ -6,10 +6,12 @@ import moviepy.video.io.ImageSequenceClip
 
 parser = argparse.ArgumentParser(description='Process image files into a video')
 parser.add_argument("--fps", type=float, help="How many frames per second", default=1)
+parser.add_argument("--output", type=str, help="Output filename", default='my_video.mp4')
 args = parser.parse_args()
 
 image_folder: Path = Path('test')
 fps: float = args.fps
+output_filename = args.output
 
 image_files = [str(img) for img in image_folder.glob("*.png")]
 
@@ -18,4 +20,4 @@ image_files.sort()
 print(image_files)
 
 clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
-clip.write_videofile('my_video.mp4')
+clip.write_videofile(output_filename)
